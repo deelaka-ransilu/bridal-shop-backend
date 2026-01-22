8a5ebcbce7f73cd23e0d485a58afd547ced7ba3b
Deelaka Ransilu <ransilu.deelaka@gmail.com>
2026-01-23 04:55:12 +0530

    Initial commit: Backend setup with Security and JPA

---
+++ src/main/java/edu/bridalshop/backend/BridalShopApplication.java
@@ -0,0 +1,13 @@
+package edu.bridalshop.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BridalShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BridalShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/bridalshop/backend/BridalShopApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.bridalshop.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BridalShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
