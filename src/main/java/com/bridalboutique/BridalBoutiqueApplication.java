6aa2d465a299efa8c824fff35f39787660cd80f2
Deelaka Ransilu <ransilu.deelaka@gmail.com>
2026-03-30 14:25:29 +0530

    chore: scaffold Spring Boot 3.5.1 + Maven

---
+++ src/main/java/com/bridalboutique/BridalBoutiqueApplication.java
@@ -0,0 +1,13 @@
+package com.bridalboutique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BridalBoutiqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BridalBoutiqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bridalboutique/BridalBoutiqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridalboutique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BridalBoutiqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
